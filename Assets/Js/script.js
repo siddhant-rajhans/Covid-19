cd80512080496a92772e31e378cc35cbb08da6ae
Sid <siddhantrajhans@gmail.com>
2021-06-18 03:05:56 +0530

    First commit

---
+++ Assets/Js/script.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+      $('#menu').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+
+      if($(window).scrollTop() > 0){
+        $('.scroll-top').show();
+      }else{
+        $('.scroll-top').hide();
+      }
+
+      // scroll top 
+
+      
+    $('section').each(function(){
+
+      let height = $(this).height();
+      let offset = $(this).offset().top - 200;
+      let id = $(this).attr('id');
+      let top = $(window).scrollTop();
+
+      if(top > offset && top < offset + height){
+        $('.navbar ul li a').removeClass('active')
+        $('.navbar').find(`[href="#${id}"]`).addClass('active');
+      }
+
+    });
+
+    }); 
+    // Modal image
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+    
+
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+
+      e.preventDefault();
+
+      $('html, body').animate({
+
+        scrollTop : $($(this).attr('href')).offset().top,
+
+      },
+      500,
+      'linear'
+      )
+
+    })
+
+});
+
+
+
+
